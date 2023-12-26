4963e59435c9359f8af016a65c79528b667937bf
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2023-12-26 22:11:30 +0300

    ij

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -4,13 +4,14 @@ const numbers = [
     [111, 200, 333, 400, 555],
 ];
 
-
-for (const arr of numbers) {
-    for (const number of arr) {
-        if(number % 2 !== 0){
-            console.log(number);
+let res23 = [];
+for (let i = 0; i < numbers.length; i++) {
+    res23[i]=[]
+    for (let j = 0; j < numbers[i].length; j++) {
+        if (numbers[i][j] % 2 == 0) {
+            res23[i].push(numbers[i][j])
         }
     }
 }
 
-console.log(numbers);
+console.log(res23);
